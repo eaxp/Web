fcd6cd6645fc257d3faff6865520617c671df5ad
eaxp <earthzazap.ez@gmail.com>
2021-09-21 22:27:40 +0700

    .

--- pages/showdata.vue
+++ pages/showdata.vue
@@ -1,45 +1,51 @@
 <template>
     <div>
-        <h>page show data tables</h>
-        <h2 v-if="data">{{data[0].id}}</h2>
+        <h1>page show data tables</h1><br><br><br>
+        <!--<h2 v-if="data">{{data[0].id}}</h2>-->
         
         <v-card>
             <v-card-title>
-        Employee table
-        <v-spacer></v-spacer>
-        <v-text-field
-          v-model="search"
-          append-icon="mdi-magnify"
-          label="Search"
-          single-lin
-          hide-details
-        >
-        </v-text-field>
-      </v-card-title>
-      <v-data-table
-        :headers="headers"
-        :items="data"
-        :search="search"
-        v-if="data">
-        </v-data-table>
-<br><br>
-
-
-        
-      
-        </v-card>
+                Data table
+                    <v-spacer></v-spacer>
+                        <v-text-field v-model="search" append-icon="mdi-magnify" label="Search"
+                        single-lin hide-details>
+                        </v-text-field>
+            </v-card-title>
+            <v-data-table :headers="headers" :items="data" :search="search" v-if="data">
+            </v-data-table>
+            <br><br>
+        </v-card><br><br><br>
+        <!--<v-card>
+            <v-card-title>
+                Data table 01
+                    <v-spacer></v-spacer>
+                        <v-text-field v-model="search" append-icon="mdi-magnify" label="Search"
+                        single-lin hide-details>
+                        </v-text-field>
+            </v-card-title>
+            <v-data-table :headers="headers1" :items="data1" :search="search1" v-if="data1">
+            </v-data-table>
+        </v-card>-->
     </div>
 </template>
 <script>
-const url = "/data.json";
+const url = "/datatest.json";
+const url1 ="/data.json";
 export default {
     data(){
         return{
             data: null,
+            data1: null,
+            search1: "",
             search: "",
             headers: [
                 { text: "ID", align: "start", sortable: true, value: "id" },
-                { text: "T/F", value: "t/f" },
+                { text: "Sum", value: "sum"},
+                { text: "Date", value: "date" },                
+            ],
+            headers1: [
+                { text: "ID", align: "start", sortable: true, value: "id" },
+                { text: "T/F", value: "t/f"},
                 { text: "Date", value: "time" },                
             ],
         }
@@ -49,17 +55,11 @@ export default {
         const res = await this.$axios.get(url);
         console.log(res.data);
         this.data = res.data;
+
     }
     catch (e) {
         console.error(e);
     }
   },
-  methods: {
-      getColor (calories) {
-        if (calories > 400) return 'red'
-        else if (calories > 200) return 'orange'
-        else return 'green'
-      },
-    },
 }
 </script>
--- pages/showgrahp.vue
+++ pages/showgrahp.vue
@@ -13,10 +13,10 @@
       </v-col>
       <br><br>
       <v-col md="12">
-        <v-card>
-          <bar-chart
-            :barchartdata="barchartdata"
-            :barchartoptions="barchartoptions"
+        <v-card v-if="loaded">
+          <line-chart
+            :linechartdata="jsonchartdata"
+            :lineoptions="barchartoptions"
             :height="300"
           />
         </v-card>
@@ -27,7 +27,7 @@
 </template>
 
 <script>
-const url = "/data.json";
+const url = "/datatest.json";
 
 export default {
   data() {
@@ -46,45 +46,6 @@ export default {
       options: {
         responsive: true,
         maintainAspectRatio: false
-      },
-      barchartdata: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [
-          {
-            label: "# of Votes",
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)"
-            ],
-            borderColor: [
-              "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)"
-            ],
-            borderWidth: 1
-          }
-        ]
-      },
-      barchartoptions: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true
-              }
-            }
-          ]
-        }
       },
       loaded: false,
       apichartdata: {
