fff345d56429836d35d0808050a96af220f931d1
eaxp <earthzazap.ez@gmail.com>
2021-09-26 02:01:41 +0700

    .

--- pages/login.vue
+++ pages/login.vue
@@ -1,30 +1,30 @@
 <template>
-    <div>
-        <html>
-            <head>
-                <meta charset="UFT-8">
-                <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
-                <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                <link rel="stylesheet" href="style.css">  
-            </head>
-            <body>
-                
-                    <div>
-                    <div class="blurred-box">
-                        <div class="user-login-box">
-                            <span class="user-icon"></span>
-                            <div class="user-name">Phumin</div>
-                            <input type="text" class="user-username" placeholder="Username">
-                            <input type="text" class="user-psaaeord" placeholder="Password">
-                        </div>
-                    </div>
-                </div>
-                <div class="g-signin2" data-onsuccess="onSignIn"></div>
-            </body>
-        </html>
-        
-        
+    <div id="login">
+        <input type="email" v-model="email" />
+        <input type="password" v-model="password">
+        <button @click="googleSignIn">Google sign in</button>
+
         
     </div>
 </template>
-
+<script>
+import firebase from "firebase";
+export default {
+    data(){
+        return{
+            email:"",
+            password:"",
+        };
+    },
+    methods:{
+        googleSignIn: function(){
+            const provider = new firebase.auth.GoogleAuthProvider();
+            firebase.suth().signInWithPopup(provider).then(() =>{
+                alert("Sign in!");
+            }).catch(err =>{
+                console.log(err);
+            });
+        },
+    },
+};
+</script>
