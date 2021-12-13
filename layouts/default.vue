76d5d67c01f50816ceecd5287bcb58af1b7248df
eaxp <earthzazap.ez@gmail.com>
2021-12-13 17:52:11 +0700

    .

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,41 +1,46 @@
 <template>
-  <v-app dark v-bind:style="{ background: $vuetify.theme.themes.dark.background}">
-    <v-navigation-drawer v-model="drawer" clipped fixed app color="amber lighten-3" >
-      <v-list >
-        <v-list-item  
+  <v-app
+    dark
+    v-bind:style="{ background: $vuetify.theme.themes.dark.background }"
+  >
+    <v-navigation-drawer
+      v-model="drawer"
+      clipped
+      fixed
+      app
+      color="amber lighten-3"
+    >
+      <v-list>
+        <v-list-item
           v-for="(item, i) in items"
           :key="i"
           :to="item.to"
-
           router
           exact
         >
-          <v-list-item-action style="color: #881518;">
-            <v-icon >{{ item.icon }}</v-icon>
+          <v-list-item-action style="color: #881518">
+            <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
-          <v-list-item-content >
-            <v-list-item-title v-text="item.title"/>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar clipped-left fixed app 
-    color="red darken-3">
+    <v-app-bar clipped-left fixed app color="red darken-3">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <img src="/logo_mfu.png" alt="user" />
       <v-toolbar-title v-text="title" />
 
-      <v-spacer />     
+      <v-spacer />
     </v-app-bar>
-
-    
 
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
-    </v-main>    
+    </v-main>
   </v-app>
 </template>
 
@@ -52,8 +57,8 @@ export default {
           icon: "mdi-account-details",
           title: "หน้าหลัก",
           to: "/",
-        }, 
-         {
+        },
+        {
           icon: "mdi-database",
           title: "ตารางข้อมูล",
           to: "/showdata",
@@ -66,15 +71,15 @@ export default {
         {
           icon: "mdi-circle-outline",
           title: "test01",
-          to: "/login",
+          to: "/test01",
         },
         {
           icon: "mdi-camcorder",
           title: "test02",
-          to: "/addvideo",
+          to: "/test02",
         },
       ],
-      
+
       miniVariant: false,
       right: true,
       rightDrawer: false,
