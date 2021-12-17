cee6f486bc38c93debe0b955efd89400931d0b3d
eaxp <earthzazap.ez@gmail.com>
2021-12-17 18:45:21 +0700

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,7 +27,7 @@
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
+            <v-list-item-title v-text="item.title" style="Libre Baskerville"/>
           </v-list-item-content>
         </v-list-item>
       </v-list>
